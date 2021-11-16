904ea4cdb033d616102b2b157e7bcaa6e7851112
TomConn15 <94468003+TomConn15@users.noreply.github.com>
2021-11-16 10:57:16 -0500

    Add files via upload

---
+++ PMConnolly.R
@@ -0,0 +1,16 @@
+#' PMConnolly Package
+#'
+#' Contains functions to conduct the 5 statistical procedures described in the paper " A Simple and Robust Method for partially Matched Samples Using the
+#' P-Values Pooling Approach" by Pei Fen Kuan and Bo Huang. The functions include:
+#' liptak_weighted(...),
+#' kim_mod(...),
+#' looney_jones(...),
+#' ekbohm(...), and
+#' lin_stivers(...).
+#'
+#' @docType package
+#'
+#' @author Thomas Connolly \email{thomas.j.connolly@stonybrook.edu}
+#'
+#' @name PMConnolly
+NULL
